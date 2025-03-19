1fe28f50f2240a12c70300844043a86a6b10b8f1
Mihai Gătejescu <mgatejescu@pm.me>
2025-03-19 22:13:43 +0200

    Add a button

--- PxEdit.java
+++ PxEdit.java
@@ -1,4 +1,6 @@
+import javax.swing.JButton;
 import javax.swing.JFrame;
+
 
 class PxEdit {
     public static void main(String[] args)
@@ -6,6 +8,8 @@ class PxEdit {
         JFrame frame = new JFrame("PxEdit");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(300,300);
+        JButton exit = new JButton("Exit");
+        frame.getContentPane().add(exit);
         frame.setVisible(true);
     }
 }
