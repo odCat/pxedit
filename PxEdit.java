9251f6b16d21251552d7985a6d4e6a13f4b4e4db
Mihai Gătejescu <mgatejescu@pm.me>
2025-03-19 23:25:10 +0200

    Close the app on button click

--- PxEdit.java
+++ PxEdit.java
@@ -1,15 +1,33 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
 
-class PxEdit {
+class PxEdit implements ActionListener {
+
+    private JFrame frame;
+
     public static void main(String[] args)
     {
-        JFrame frame = new JFrame("PxEdit");
+        PxEdit editor = new PxEdit();
+        editor.start();
+    }
+
+    private void start()
+    {
+        this.frame = new JFrame("PxEdit");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(300,300);
+
         JButton exit = new JButton("Exit");
+        exit.addActionListener(this);
         frame.getContentPane().add(exit);
+
         frame.setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent event) {
+        this.frame.dispose();
     }
 }
